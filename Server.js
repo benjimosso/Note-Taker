e62c001d2b53c3e2f7804e99dadb98ba3ced4cea
Daniel Mosso <dmosso@DataHardwareDepot.local>
2021-06-15 10:24:09 -0700

    Port issue

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const { createBrotliCompress } = require('zlib');
 // Sets up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes')
